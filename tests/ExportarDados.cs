0fa160f9adaeb7a989f88b3410ab06c40c4fe46d
Radhanama <radhanama@hotmail.com>
2025-06-16 16:45:10 -0300

    Add using directives support

--- tests/ExportarDados.cs
+++ tests/ExportarDados.cs
@@ -1,3 +1,33 @@
+using System.Collections;
+using System.ComponentModel;
+using ShineOn.Rtl;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using SGU.Business;
+using SGU.Controllers;
+using MetaBuilders.WebControls;
+using ShineOn.Vcl;
+using C1.Win.C1Report;
+using c1.Web.C1WebReport;
+using log4net;
+using System.Configuration;
+using System.Globalization;
+using System.Threading;
+using System.Xml;
+using System.IO;
+using log4net.Config;
+using c1.C1Zip;
+using System.Net;
+using SGU.Data.Web;
+using BarcodeNETWorkShop;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace SGUWeb {
     public partial class ExportarDados : TPageBasico2 {
         public void Page_Load(object sender, EventArgs e) {
