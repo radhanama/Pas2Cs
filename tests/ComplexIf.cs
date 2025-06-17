16698316cf3af3213c180d1f32bbab9c9ffe4445
Radhanama <radhanama@hotmail.com>
2025-06-17 16:42:03 -0300

    Group loops and generics tests

--- tests/ComplexIf.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Demo {
-    public partial class ComplexIf {
-        public static void Check(object v, Dictionary<string, int> dict, int[] arr) {
-            if ((int)v > dict["foo"] && arr[0] < dict.Count && dict.ContainsKey("bar") || arr.Length > 0) Console.WriteLine("ok");
-        }
-    }
-}
--- tests/GenericMethod.cs
+++
@@ -1,6 +0,0 @@
-namespace Demo {
-    public partial class GenClass {
-        public static void DeSerialize<T>(string json, T obj) {
-        }
-    }
-}
