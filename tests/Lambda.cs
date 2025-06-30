620495f12090a50a099807cd92889ede447e9245
Radhanama <radhanama@hotmail.com>
2025-06-30 15:46:51 -0300

    Handle untyped lambda parameters

--- tests/Lambda.cs
+++ tests/Lambda.cs
@@ -2,7 +2,8 @@ namespace Demo {
     public partial class LambdaClass {
         public bool Run() {
             var predicate = i => i > 0;
-            return predicate(5);
+            var eq = (a, b) => a == b;
+            return predicate(5) && eq(5, 5);
         }
     }
 }
