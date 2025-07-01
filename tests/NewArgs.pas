e55f037247642f0df8c75b2b8207cf81695affb3
Radhanama <radhanama@hotmail.com>
2025-07-01 08:09:06 -0300

    Handle named args in new expressions

--- tests/NewArgs.pas
+++ tests/NewArgs.pas
@@ -4,6 +4,7 @@ type
   Foo = public class
   public
     class method Make;
+    class method Named;
   end;
 
 implementation
@@ -12,6 +13,12 @@ class method Foo.Make;
 var t : System.Collections.Generic.List<Integer>;
 begin
   t := new System.Collections.Generic.List<Integer>(5);
+end;
+
+class method Foo.Named;
+var v : Demo.Bar;
+begin
+  v := new Demo.Bar(value := 1, name := 'a');
 end;
 
 end.
