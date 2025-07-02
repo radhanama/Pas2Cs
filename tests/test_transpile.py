65f76f553a25e4369a490c83702390e21c09d562
Radhanama <radhanama@hotmail.com>
2025-07-02 09:13:22 -0300

    support inline comments in expressions

--- tests/test_transpile.py
+++ tests/test_transpile.py
@@ -59,6 +59,13 @@ class TranspileTests(unittest.TestCase):
         self.assertEqual(result.strip(), expected)
         self.assertEqual(todos, [])
 
+    def test_expr_inline_comment(self):
+        src = Path('tests/ExprInlineComment.pas').read_text()
+        expected = Path('tests/ExprInlineComment.cs').read_text().strip()
+        result, todos = transpile(src)
+        self.assertEqual(result.strip(), expected)
+        self.assertEqual(todos, [])
+
     def test_file_header(self):
         src = Path('tests/FileHeader.pas').read_text()
         expected = Path('tests/FileHeader.cs').read_text().strip()
